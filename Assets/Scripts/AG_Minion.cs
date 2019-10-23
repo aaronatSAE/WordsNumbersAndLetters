826c460ff7d3db1eac70cf89b0dbe876e365608a
Aaron G <a.goss@sae.edu>
2019-10-23 16:01:51 +1100

    Created a turret that rotates and minions that move toward the turret.

---
+++ Assets/Scripts/AG_Minion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This enemy will move toward the turret and dies when shot.
+/// </summary>
+public class AG_Minion : MonoBehaviour
+{
+    public GameObject turret;
+    public float speed = 5;
+
+    private void Start()
+    {
+        turret = GameObject.Find("Turret");
+    }
+
+    void Update()
+    {
+        // LOOK AT TURRET
+        transform.LookAt(turret.transform.position);
+
+        // MOVE
+        transform.position += transform.forward * Time.deltaTime * speed;
+    }
+}
---
+++ Assets/Scripts/AG_Turret.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Rotates toward mouse position and shoots when you click.
+/// </summary>
+public class AG_Turret : MonoBehaviour
+{
+    void Update()
+    {
+        // ROTATE TO MOUSE POSITION
+        // The information of what our raycast has hit.
+        RaycastHit hit;
+        // Create a raycast to point at the mouse's position.
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        // If our raycast hits an object...
+        if(Physics.Raycast(ray, out hit, 100))
+        {
+            //transform.LookAt(hit.transform.position);
+            transform.LookAt(new Vector3(hit.point.x, 0, hit.point.z));
+        }
+
+        // SHOOT WHEN MOUSE BUTTON IS CLICKED
+
+    }
+}
